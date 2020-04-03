eae2803c8e84a200b7babd480079580da2e9013a
Miguel Junior <56236118+miguelarcjr@users.noreply.github.com>
2020-04-03 19:13:09 -0400

    Artigo adicionado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
